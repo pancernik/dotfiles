c1a13a5144ba3fb4e07f5c2f32a56a7e4be9eb74
Rafal Wicha <rafalwicha@gmail.com>
2018-09-27 11:34:11 +0100

    Now it works on Ubuntu

---
+++ install.apt.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "Removing..."
+sudo apt --assume-yes remove vim zsh tmux curl 
+sudo apt --assume-yes autoremove
+
+echo "Installing..."
+sudo apt --assume-yes install vim zsh tmux curl 
+
+
+
